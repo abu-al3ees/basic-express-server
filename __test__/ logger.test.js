3d87b56a923142972b737e0eb6ba2d8522a10679
abu-al3ees <malkhazali83@gmail.com>
2021-05-03 23:51:50 +0300

    lab2

--- __test__/ logger.test.js
+++ __test__/ logger.test.js
@@ -3,25 +3,24 @@ const logger = require('../src/middleware/ logger.js');
 // spy on calling the console log or not
 
 describe('Logger middleware', ()=> {
-    let consoleSpy;
-    let req = {};
-    let res = {};
-    let next = jest.fn();
-    // when I use logger -> console.log has been called
-    // hook for tests
-    beforeEach(()=> {
-        consoleSpy = jest.spyOn(console, 'log').mockImplementation();
-    });
+  let consoleSpy;
+  let req = {};
+  let res = {};
+  let next = jest.fn();
+  
+  beforeEach(()=> {
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+  });
     
-    // after the tests
-    afterEach(()=> {
-        consoleSpy.mockRestore();
-    });
+ 
+  afterEach(()=> {
+    consoleSpy.mockRestore();
+  });
 
-    it('logs the output', ()=> {
-        logger(req, res, next);
-        expect(consoleSpy).toHaveBeenCalled();
-        expect(next).toHaveBeenCalled();
-    });
+  it('logs the output', ()=> {
+    logger(req, res, next);
+    expect(consoleSpy).toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
     
 });
